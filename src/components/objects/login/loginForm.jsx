5b361b8c0fca27509dff1b6e826e130a663d1969
Carlos Munoz <cbustamente@kvalue.cl>
2021-05-30 10:40:34 -0400

    delete unused import

--- src/components/objects/login/loginForm.jsx
+++ src/components/objects/login/loginForm.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-// import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import "./styles/loginForm.css";
 import jwt from "jwt-simple";
